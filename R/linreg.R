1a84a5964d3d2cbf6250c67329a83c92c9bea4b2
Chitran1987 <ghosal.chitran@gmail.com>
2020-02-20 22:46:49 +0100

    1.Updated the linreg function documentation 2.Introduced two datasets to test the linreg function (one invertible) and (the other singular) displaying an issue with multicollinearity

--- R/linreg.R
+++ R/linreg.R
@@ -8,10 +8,23 @@
 #' column of the independent variables in the perictor equation while the the others are presumed to be
 #' the dependent variables.
 #'
-#' @return A dataframe comprising of your the estimated values of the regression weights
+#' @return A dataframe comprising of your the estimated values of the regression weights if the input
+#' data does not suffer from multicollinearity
 #' @export
 #'
 #' @examples
+#' linreg(reg_data1)
+#' [,1]
+#' [1,] 2.950339
+#' [2,] 5.052787
+#' [3,] 0.999999
+#' [4,] 9.599983
+#' (The reg_data1 dataset is available with the "StatsChitran" package)
+#'
+#' linreg(reg_data2)
+#' NULL
+#' (The reg_data2 dataset is available with "StatsChitran" package. The function returns NULL in case
+#' is an issue with multicollinearity in the input dataset)
 linreg<-function(df){
   m<-matrix(NA, ncol=dim(df)[2], nrow = dim(df)[2])
   for (i in 1:(dim(m)[1])) {
