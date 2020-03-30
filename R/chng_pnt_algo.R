b1c9abb5526cea784e6838ed1de1a70903a0e64a
Chitran1987 <ghosal.chitran@gmail.com>
2020-03-31 00:39:44 +0200

    New function chng_pnt_algo added to package StatsChitran

---
+++ R/chng_pnt_algo.R
@@ -0,0 +1,92 @@
+chng_pnt_algo<-function(v,dat){
+  names(dat)<-c('V1', 'V2')
+
+  #######
+  #######create a function for accepting the profile that you want to create
+  prof_struct<-function(v,dat){
+    tgt<-dat
+    L<-length(v)
+    Bp<-v[seq(1,(2*(L-1)/3))]####number of breakpoints
+    H<-v[seq(((2*L+1)/3),L)] ####number of plateau heights
+    for (i in 1:length(Bp)) {
+      if(i==1){
+        tgt$V2[tgt$V1<=Bp[i]]<-H[i]
+      }#if close
+      else if(i==length(Bp)){
+        tgt$V2[tgt$V1>=Bp[i]]<-H[length(H)]
+        tgt$V2[tgt$V1>=Bp[i-1] & tgt$V1<=Bp[i]]<-((H[(i/2)+1]-H[i/2])/(Bp[i]-Bp[i-1]))*(tgt$V1[tgt$V1>=Bp[i-1] & tgt$V1<=Bp[i]]-Bp[i-1])+H[i/2]
+      }#else if close
+      else{
+        if(i%%2==0 ){
+          tgt$V2[tgt$V1>=Bp[i-1] & tgt$V1<=Bp[i]]<-((H[(i/2)+1]-H[i/2])/(Bp[i]-Bp[i-1]))*(tgt$V1[tgt$V1>=Bp[i-1] & tgt$V1<=Bp[i]]-Bp[i-1])+H[i/2]
+        }#if close
+        else{
+          tgt$V2[tgt$V1>=Bp[i-1] & tgt$V1<=Bp[i]]<-H[(i+1)/2]
+        }#small else close
+      }#bigelse close
+    }#forloopclose
+    return(tgt)
+  }   #function close
+  ####################################################
+
+
+  plot(dat$V1, dat$V2)
+  dat1<-prof_struct(v,dat)
+  lines(dat1$V1,dat1$V2, col='red')
+
+  ######setup the constraint matrix and the vector
+  ###number of points#####number of constraints=n-1(for breakpoints)+2*((length(v)+2)/3)(for the plateau heights)
+  n<-2*(length(v)-1)/3  ####number of breakpoints
+  #######constraint matrix
+  m<-matrix(0,nrow = 2*n+1, ncol = length(v))
+  #####loop for the n-1 breakpoint constraints
+  for (i in 1:n-1) {
+    m[i,i]<--1
+    m[i,i+1]<-1
+  }
+  #####loop for the 2*((length(v)+2)/3) plateau height constraints
+  for(i in n:(n+((n/2)+1)-1)){
+    m[i,i+1]<-1
+  }
+  for(i in ((n+((n/2)+1)-1)+1):(2*n+1) ){
+    m[i,i-((n/2)+1)+1]<--1
+  }
+  ###constraint vector
+  #####length of constraint vector is equal to no. of constraints
+  b<-rep(0L, 2*n+1)
+  #####use zeros for the breakpoint constraints
+  for(i in 1:n-1){
+    b[i]<-0
+  }
+  for (i in n:(n+((n/2)+1)-1)) {
+    b[i]<-min(dat$V2)
+  }
+  for (i in ((n+((n/2)+1)-1)+1):(2*n+1)) {
+    b[i]<--max(dat$V2)
+  }
+  #######write down the constraint function#####
+  f<-function(v){
+    dat2<-dat
+    tgt2<-prof_struct(v,dat2)
+    e<-sum((tgt2$V2-dat$V2)^2)
+    return(e)
+  }
+  r<-constrOptim(v,f,NULL,m,b)
+  v<-r$par
+  tgt<-prof_struct(v,dat)
+  lines(tgt$V1,tgt$V2, col='blue')
+
+  #######convergence factor##########
+  con<-vector(mode = 'character', length = 1)
+  if(r$convergence==0){
+    con<-'solution has converged'
+  }
+  else{
+    con<-'solution has not converged'
+  }
+  ##################################
+
+  ######result of final convergence#####
+  final<-list(r,v, con)
+  return(final)
+}
