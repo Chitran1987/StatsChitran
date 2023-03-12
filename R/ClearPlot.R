ab3d98596f5d00a5e6324bd656a3fb777c2d5147
Chitran <ghosal.chitran@gmail.com>
2023-03-12 22:30:23 +0100

    The dev.off() function in ClearPlot() was wrapped in invisible(X) to quieten the output.

--- R/ClearPlot.R
+++ R/ClearPlot.R
@@ -1,7 +1,7 @@
 #write a program to clear the graphics output if a plot is displayed
 ClearPlot<-function(){
   if (names(dev.cur()) !='null device'){
-    dev.off()
+    invisible(dev.off())
   }
   #return NULL
 }
