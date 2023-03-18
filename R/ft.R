4ed3c14055ac809c71bde23372276dc50c1408d0
Chitran <ghosal.chitran@gmail.com>
2023-03-18 12:25:12 +0100

    Nyquist creterion was set for the the ft function

--- R/ft.R
+++ R/ft.R
@@ -1,5 +1,5 @@
 #write a function to accept two vectors and find the fourier transform of one w.r.t the other
-ft <- function(X, Y, w = F){
+ft <- function(X, Y, w = F, set.nyquist = T){
   if(length(X)!=length(Y)){
     stop('X and Y have different lengths')
   }else if(is.numeric(X)&is.numeric(Y) == F){
@@ -9,10 +9,10 @@ ft <- function(X, Y, w = F){
   }else if(sum(is.na(Y)) != 0){
     stop('remove NAs from Y')
   }else{ if(sum(diff(diff(X))) != 0){
-    warning('non uniform sampling in X: Mean of sampling error will be used to calculate sampling rate')
+    warning('non uniform sampling in X: Mean of sampling error vector will be used to calculate sampling rate')
     }
     #calculate sampling freq and establish freq vector
-    rn <- diff(X) #calculate the sampling difference vector
+    rn <- diff(X) #calculate the sampling error vector
     if(w == T){
       fs <- 2*pi/mean(rn)
       wf <- seq(0, fs, by=2*pi/((max(X))-(min(X))))
@@ -24,9 +24,12 @@ ft <- function(X, Y, w = F){
       fy <- fft(Y)
       df <- data.frame(fr,fy)
     }
-    #fy <- fft(Y)
-    #df <- data.frame(fr,fy)
-    return(df)
+    if(set.nyquist==F){
+      return(df)
+    }else{
+      df <- subset(df,df[,1] <= max(df[,1])/2)  ##implement the nyquist criterion when the set.nyquist is left at default
+      return(df)
+    }
   }
 }
 
