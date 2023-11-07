6423e8f824a25a94593c2b22a214944ca781e375
Chitran <ghosal.chitran@gmail.com>
2023-11-07 20:23:11 +0100

    Changes made to movavg program for the edges of the averager

--- R/movavg.R
+++ R/movavg.R
@@ -1,15 +1,15 @@
 movavg <- function(X,Y, bn, fn){
   if( (length(bn) != 1)|(length(fn) != 1) ){
     stop('Inputs to fn and fb should be numeric and single digit')
+  }else if(length(X) != length(Y)){
+    stop('length of input vectors X and Y need to be the same')
   }else{
-    y_tmp <- c(rep(0,bn), Y, rep(0,fn))
     y_res <- NULL
-    for (i in (bn+1):(bn+length(Y))) {
-      y_tmp2 <- mean(y_tmp[(i-bn):(i+fn)])
-      y_res <- c(y_res, y_tmp2)
+    for (i in 1:length(Y)) {
+      y_res <- c( y_res, sum( Y[max(1,i-bn): min(i+fn, length(Y))] )/( min(i+fn, length(Y)) - max(1, i-bn) + 1 ) )
     }
-    df <- data.frame(X,y_res)
-    names(df) <- c('X','Y')
-    return(df)
   }
+  df <- data.frame(X,y_res)
+  names(df) <- c('X','Y')
+  return(df)
 }
