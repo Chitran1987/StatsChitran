3845029852200d12c04c185844c521a02fd68161
Chitran Ghosal <ghosal.chitran@gmail.com>
2024-04-19 18:37:43 +0200

    New file rad2deg.R uploaded to github

---
+++ R/rad2deg.R
@@ -0,0 +1,34 @@
+#write a code to convert radians to degrees
+rad2deg <- function(rad, overlap = F){
+  if(overlap == F){
+    return((180/pi)*rad)
+  }else{
+    bin_tab <- rad >= 0
+    res_vec <- vector(mode = 'numeric', length = length(bin_tab))
+    for (i in 1:length(bin_tab)) {
+      if( bin_tab[i] == T){
+        a <- (180/pi)*rad[i]
+        rem <- a%%180
+        quo <- floor(a/180)
+        if(quo%%2 == 0){
+          res_vec[i] <- rem
+        }else{
+          res_vec[i] <- rem - 180
+        }
+
+      }else{
+        a <- (180/pi)*rad[i]
+        a <- abs(a)
+        rem <- a%%180
+        quo <- floor(a/180)
+        if(quo%%2 == 0){
+          res_vec[i] <- -1*rem
+        }else{
+          res_vec[i] <- -1*(rem - 180)
+        }
+      }
+    }
+
+  }
+  return(res_vec)
+}
