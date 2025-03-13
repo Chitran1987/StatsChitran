a3ba245f5c23b9cec8215c0a0d4c49992b396cfb
Chitran1987 <ghosal.chitran@gmail.com>
2025-03-13 11:23:46 +0100

    mat.mask mask algorithm updated

--- R/mat.mask.R
+++ R/mat.mask.R
@@ -24,7 +24,7 @@ mat.mask <- function(org, mask, drop.dat=F){
     return(org*mask)
   }else{
     ####generate matrix M
-    M <- org*mask
+    #M <- org*mask
     #### Sum each row in matrix M
     #### indexes of rows where rowsums are not equal to zero
     #### If min(ind) > 1, then remove rows with indexes 1:(ind - 1)
@@ -32,32 +32,72 @@ mat.mask <- function(org, mask, drop.dat=F){
     #### remove the rows with those indexes
     #### Then proceed to do the same with Columns
     #### This will draw a box and give us the minimum size of the image that we must keep to have the requisite image
+
+    M <- org*mask
     ##This is the rows part
-    RSums <- rowSums(M)
-    ind <- which(RSums != 0)
-    N <- nrow(M)
-    if( min(ind) > 1 ){
-      M <- M[ - (1:(min(ind) - 1) ), ]
+    row_res <- apply(mask, 1, any)
+    ####while loop counting front eliminations
+    dmmy.ctr <- F
+    i <- 1
+    while (dmmy.ctr == F) {
+      dmmy.ctr <- dmmy.ctr | row_res[i]
+      i <- i + 1
     }
-    RSums <- rowSums(M)
-    ind <- which(RSums != 0)
-    N <- nrow(M)
-    if(max(ind) < N){
-      M <- M[-( (max(ind)+1):N ),]
+    frnt_elm <- i-2
+    ####eliminate from the front
+
+    if(frnt_elm > 0){
+      M <- M[-(1:frnt_elm),]
+      mask <- mask[-(1:frnt_elm),]
     }
+    ####while loop counting back eliminations
+    row_res <- apply(mask, 1, any)
+    dmmy.ctr <- F
+    i <- length(row_res)
+    while (dmmy.ctr == F) {
+      dmmy.ctr <- dmmy.ctr | row_res[i]
+      i <- i - 1
+    }
+    bck_elm <- length(row_res)-i-1
+
+    ####eliminate from the back
+    if(bck_elm > 0){
+      M <- M[-((length(row_res)-bck_elm+1):length(row_res) ),]
+      mask <- mask[-((length(row_res)-bck_elm+1):length(row_res) ),]
+    }
+
     ##This is the columns part
-    CSums <- colSums(M)
-    ind <- which(CSums != 0)
-    N <- ncol(M)
-    if( min(ind) > 1 ){
-      M <- M[ ,- (1:(min(ind) - 1) ) ]
+    rm(row_res)
+    col_res <- apply(mask, 2, any)
+    ####while loop counting front eliminations
+    dmmy.ctr <- F
+    i <- 1
+    while (dmmy.ctr == F) {
+      dmmy.ctr <- dmmy.ctr | col_res[i]
+      i <- i + 1
     }
-    CSums <- colSums(M)
-    ind <- which(CSums != 0)
-    N <- ncol(M)
-    if(max(ind) < N){
-      M <- M[,-( (max(ind)+1):N )]
+    frnt_elm <- i-2
+    ####eliminate from the front
+    if(frnt_elm > 0){
+      M <- M[,-(1:frnt_elm)]
+      mask <- mask[,-(1:frnt_elm)]
     }
+    ####while loop counting back eliminations
+    col_res <- apply(mask, 2, any)
+    dmmy.ctr <- F
+    i <- length(col_res)
+    while (dmmy.ctr == F) {
+      dmmy.ctr <- dmmy.ctr | col_res[i]
+      i <- i - 1
+    }
+    bck_elm <- length(col_res)-i-1
+
+    ####eliminate from the back
+    if(bck_elm > 0){
+      M <- M[,-((length(col_res)-bck_elm+1):length(col_res) )]
+      mask <- mask[,-((length(col_res)-bck_elm+1):length(col_res) )]
+    }
+
     return(M)
   }
 }
