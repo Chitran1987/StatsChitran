bd2f74730064fbb6b6432f3b9fc5c31f4eb8997a
Chitran1987 <ghosal.chitran@gmail.com>
2025-06-18 15:42:38 +0200

    vectorized ArithProg code

--- R/ArithProg.R
+++ R/ArithProg.R
@@ -1,9 +1,6 @@
 #write an arithmetic progression with st as start point, d as the difference and n as no. of points
 ArithProg<-function(st=0, n, d){
-  v<-vector(mode = 'numeric', length = n)
-  for (i in 1:n) {
-    v[i]<-st+(i-1)*d
-  }
+  v <- seq(from = st, by = d, length.out = n)
   return(v)
 }
 
