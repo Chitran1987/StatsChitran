57112c5b3175f71a59b8aa10bb26775f531fc8d6
Chitran1987 <ghosal.chitran@gmail.com>
2025-06-18 14:28:32 +0200

    GeomProg code vectorization attempt

--- R/GeomProg.R
+++ R/GeomProg.R
@@ -1,8 +1,6 @@
 #write a geometric progression with st as start point, r as the difference and n as no. of points
 GeomProg<-function(st=1,r,n){
-  v<-vector(mode = 'numeric', length = n)
-  for (i in 1:n) {
-    v[i]<-st*(r^(i-1))
-  }
+  v <- seq(0, n-1)
+  v <- st*r^v
   return(v)
 }
