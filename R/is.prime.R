3444a19f2006086959b6da4d606f8ae3d24dabe2
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-25 04:13:59 +0200

    created is.prime.R

---
+++ R/is.prime.R
@@ -0,0 +1,16 @@
+##Write a function to check whether a no. is a prime no. or not
+is.prime <- function(vec){
+
+
+  ###error check
+  if(!is.numeric(vec)){
+    stop('vec has to be a numeric vector')
+  }
+  if(!all(vec%%1 == 0)){
+    stop('vec can contain only positive integers')
+  }
+  if(!all(vec > 0)){
+    stop('vec can only have positive integers')
+  }
+  retrun(is_prime(vec))
+}
