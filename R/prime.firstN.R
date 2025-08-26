edf2727b5fd2de38ab2cd6d88eb286f3a21a8fcd
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-26 22:46:34 +0200

    prime.firstN R wrapper succesfully built.

---
+++ R/prime.firstN.R
@@ -0,0 +1,7 @@
+#Write a script to find the first N prime nos
+prime.firstN <- function(N){
+  if( !(N%%1 == 0) | (length(N) != 1) ){
+    stop('N has to be a single integer')
+  }
+  return(prime_firstN(N))
+}
