cf50cd989d03ba1ebd85041bd5f7da9fb567b183
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-26 22:48:23 +0200

    Changes made

--- R/prime.firstN.R
+++ R/prime.firstN.R
@@ -1,7 +1,7 @@
 #Write a script to find the first N prime nos
 prime.firstN <- function(N){
-  if( !(N%%1 == 0) | (length(N) != 1) ){
-    stop('N has to be a single integer')
+  if( !(N%%1 == 0) | (length(N) != 1) | ( N < 1 ) ){
+    stop('N has to be a single and positive integer')
   }
   return(prime_firstN(N))
 }
