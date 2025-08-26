a876c6820b467d38782c0b72c9a72b819da8978c
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-26 15:30:48 +0200

    Some changes

--- R/StatsChitran-package.R
+++ R/StatsChitran-package.R
@@ -1,5 +1,10 @@
-#' StatsChitran
-#' @keywords internal
+#' StatsChitran: Utilities for <your short tagline>
+#'
+#' Package-level documentation.
+#'
+#' @docType package
+#' @name StatsChitran
 #' @useDynLib StatsChitran, .registration = TRUE
 #' @importFrom Rcpp evalCpp
+#' @exportPattern ^[^.].*
 "_PACKAGE"
