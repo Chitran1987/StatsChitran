1d3bfb1fbb3acedba2aace6b1f38f991ac41ebcf
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-02 18:15:17 +0200

    Updated

--- src/prime_helpers.h
+++ src/prime_helpers.h
@@ -3,5 +3,6 @@
 
 bool primefunc(int n); //just a declaration as a helper function
 int primefunc_int(int n);//just a declaration as a helper function
+int primefunc_large_int(uint64_t n); //just a declaration as a helper function
 
 #endif
