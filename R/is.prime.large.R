b5da461f613e19d5d159113c1ec658059fa73aaa
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-02 17:51:02 +0200

    is.prime.large.R

---
+++ R/is.prime.large.R
@@ -0,0 +1,18 @@
+##Write a function for computing which elements in a LARGE list contains prime nos
+is.prime.large <- function(vec){
+  ###error check
+  if(!is.numeric(vec)){
+    stop('vec has to be a numeric vector')
+  }
+  if(!all(vec%%1 == 0)){
+    stop('vec can contain only positive integers')
+  }
+  if(!all(vec > 0)){
+    stop('vec can only have positive integers')
+  }
+  if(length(vec) < 10^7){
+    return(is_prime(vec))
+  }else{
+    return(is_prime_large(vec))
+  }
+}
