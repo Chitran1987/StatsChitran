fe76036850492ba08771a3a4747e7df51b300b1c
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-05 04:24:39 +0200

    created

---
+++ src/calculus_helpers.h
@@ -0,0 +1,10 @@
+#ifndef CALCULUS_HELPERS_H
+#define CALCULUS_HELPERS_H
+
+#include <Rcpp.h>
+
+double num_integrate_cpp(Rcpp::NumericVector X, Rcpp::NumericVector Y, double xmin, double xmax); //just a declaration as a helper function
+//int primefunc_int(int n);//just a declaration as a helper function
+//int primefunc_large_int(uint64_t n); //just a declaration as a helper function
+
+#endif
