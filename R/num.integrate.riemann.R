5dea873be14933d19840bda0502c020491f80052
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-07 02:33:35 +0200

    New R function

---
+++ R/num.integrate.riemann.R
@@ -0,0 +1,32 @@
+##Write a function to compute Riemann sums between two fixed values
+num.integrate.riemann <- function(X, Y, xmin, xmax){
+  ##error handling
+  if(xmin < min(X)){
+    stop('Lower bound of integration not in domain')
+  }
+  if(xmax > max(X)){
+    stop('Upper bound of integration not in domain')
+  }
+  if(length(X) != length(Y)){
+    stop('X and Y are not of the same lengths')
+  }
+  if(!is.numeric(X) | !is.numeric(Y) ){
+    stop('X and Y need to be numeric vectors')
+  }
+  if(xmax == xmin){
+    return(0)
+  }
+  if(abs(xmax - xmin) < 4*mean(diff(X))){
+    stop('Not enough samples to measure an integral')
+  }
+  if(length(X) < 4){
+    stop('Not enough samples with probably non-uniform sampling')
+  }
+  if(length(X) <= 5*10^5){
+    s = RSum(X, Y, xmin, xmax)
+  }else{
+    s = RSum_par(X, Y, xmin, xmax)
+  }
+  return(s)
+}
+
