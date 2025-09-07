c5d59d73b2575fc3a60ac11f446fe7fbd4beeb5a
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-07 02:34:19 +0200

    updated

--- src/calculus_helpers.h
+++ src/calculus_helpers.h
@@ -4,6 +4,7 @@
 #include <Rcpp.h>
 
 double num_integrate_cpp(Rcpp::NumericVector X, Rcpp::NumericVector Y, double xmin, double xmax); //just a declaration as a helper function
+double RSum(Rcpp::NumericVector X, Rcpp::NumericVector Y, double xmin, double xmax); //helper function declaration
 //int primefunc_int(int n);//just a declaration as a helper function
 //int primefunc_large_int(uint64_t n); //just a declaration as a helper function
 
