74cc525e6e0d612061c1e860273d3d9cf4c2b888
Chitran1987 <ghosal.chitran@gmail.com>
2025-09-20 01:09:53 +0200

    Update it with c++ backend

--- R/GeomProg.R
+++ R/GeomProg.R
@@ -1,6 +1,16 @@
 #write a geometric progression with st as start point, r as the difference and n as no. of points
 GeomProg<-function(st=1,r,n){
-  v <- seq(0, n-1)
-  v <- st*r^v
-  return(v)
+  ####error handling
+  if( !is.numeric(st) || length(st)!=1 ){
+    stop('st should be a numeric scalar')
+  }
+  if( !is.numeric(r) || length(r)!=1 ){
+    stop('d should be a numeric scalar')
+  }
+  if( !is.numeric(n) || length(n)!=1 || n%%1 != 0 || n <= 1){
+    stop('n should be an integer scalar greater than one')
+  }
+
+  return(Geom_Prog(st, r, n))
 }
+
