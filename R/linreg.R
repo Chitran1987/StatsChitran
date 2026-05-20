cf31ac5d474ad1e698561dbe7bbad7d75671a174
Chitran Ghosal <ghosal.chitran@gmail.com>
2026-05-21 01:58:33 +0200

    linreg.R function and documentation cleanup.

--- R/linreg.R
+++ R/linreg.R
@@ -1,71 +1,87 @@
-#minimize the residual sum of squares
-#multicolinearity of input variables is a problem and should be addressed in the help files/help links
-
-
-#' function for calculating the linear regression
+#' Fit a multiple linear regression model using the normal equations
 #'
-#' @param df input a dataframe with more than two columns. The last column is always presumed to be the
-#' column of the independent variables in the perictor equation while the the others are presumed to be
-#' the dependent variables.
+#' `linreg()` estimates the coefficients of a multiple linear regression model
+#' using the normal-equation form of ordinary least squares. The input must be a
+#' data frame in which the last column is treated as the dependent/response
+#' variable and all preceding columns are treated as independent/predictor
+#' variables.
 #'
-#' @return A dataframe comprising of your the estimated values of the regression weights if the input
-#' data does not suffer from multicollinearity
-#' @export
+#' The function constructs the normal-equation matrix manually and solves for the
+#' regression coefficients. An intercept term is included automatically. If the
+#' normal-equation matrix is computationally singular, which can occur for
+#' example when the predictor variables suffer from strong multicollinearity, the
+#' function returns `NULL`.
+#'
+#' @param df A data frame containing the regression data. The last column is
+#'   interpreted as the dependent/response variable. All preceding columns are
+#'   interpreted as independent/predictor variables.
+#'
+#' @return A numeric vector containing the estimated regression coefficients.
+#'   The first coefficients correspond to the predictor variables in the order
+#'   in which they appear in `df`. The final coefficient is the intercept term.
+#'   If the normal-equation matrix is computationally singular, the function
+#'   returns `NULL`.
+#'
+#' @details
+#' This function implements ordinary least squares by explicitly constructing and
+#' solving the normal equations. Because the method relies on matrix inversion,
+#' the calculation may fail or become unstable when the predictor variables are
+#' linearly dependent or nearly linearly dependent. In such cases, the determinant
+#' of the normal-equation matrix becomes very small and the function returns
+#' `NULL`.
 #'
 #' @examples
 #' linreg(reg_data1)
-#' [,1]
-#' [1,] 2.950339
-#' [2,] 5.052787
-#' [3,] 0.999999
-#' [4,] 9.599983
-#' (The reg_data1 dataset is available with the "StatsChitran" package)
+#'
+#' # The dataset reg_data1 is included with the StatsChitran package.
+#' # Example output:
+#' # [1] 2.950339 5.052787 0.999999 9.599983
 #'
 #' linreg(reg_data2)
-#' NULL
-#' (The reg_data2 dataset is available with "StatsChitran" package. The function returns NULL in case
-#' is an issue with multicollinearity in the input dataset)
-linreg<-function(df){
-  m<-matrix(NA, ncol=dim(df)[2], nrow = dim(df)[2])
+#'
+#' # The dataset reg_data2 is included with the StatsChitran package.
+#' # This example returns NULL when the input data suffer from multicollinearity.
+#'
+#' @export
+linreg <- function(df) {
+  m <- matrix(NA, ncol = dim(df)[2], nrow = dim(df)[2])
+
   for (i in 1:(dim(m)[1])) {
     for (j in 1:(dim(m)[1])) {
-      if(i!=dim(m)[1] & j!=dim(m)[2]){
-        m[i,j]<-sum((df[,i])*(df[,j]))
-      }
-      else{
-        if(i==dim(m)[1] & j==dim(m)[1]){
-          m[i,j]<-dim(df)[1]
-        }
-        else{
-          if(i==dim(m)[1]){
-            m[i,j]<-sum(df[,j])
-          }
-          else{
-            m[i,j]<-sum(df[,i])
+      if (i != dim(m)[1] & j != dim(m)[2]) {
+        m[i, j] <- sum((df[, i]) * (df[, j]))
+      } else {
+        if (i == dim(m)[1] & j == dim(m)[1]) {
+          m[i, j] <- dim(df)[1]
+        } else {
+          if (i == dim(m)[1]) {
+            m[i, j] <- sum(df[, j])
+          } else {
+            m[i, j] <- sum(df[, i])
           }
         }
       }
-
     }
-
   }
-  #creating the vector
-  v<-vector(mode = 'numeric', length =dim(m)[1] )
+
+  # creating the vector
+  v <- vector(mode = "numeric", length = dim(m)[1])
+
   for (i in 1:length(v)) {
-    if(i!=length(v)){
-      v[i]<-sum((df[,i])*(df[,length(v)]))
-    }
-    else{
-      v[i]<-sum(df[,i])
+    if (i != length(v)) {
+      v[i] <- sum((df[, i]) * (df[, length(v)]))
+    } else {
+      v[i] <- sum(df[, i])
     }
   }
-  b<-matrix(v, ncol = 1)
-  if(det(m)<10^{-15}){
+
+  b <- matrix(v, ncol = 1)
+
+  if (det(m) < 10^(-15)) {
+    message("matrix computationally singular")
     return(NULL)
-    print('matrix computationally singular')
-  }
-  else{
-    res<-as.vector(solve(m,b))
+  } else {
+    res <- as.vector(solve(m, b))
     return(res)
   }
 }
