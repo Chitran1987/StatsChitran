fac9d70640330d80e4ac17de95a5c94aff8522cd
Chitran Ghosal <ghosal.chitran@gmail.com>
2026-06-06 15:25:17 +0200

    Updated exp.prob with x_0 param

--- R/exp.prob.R
+++ R/exp.prob.R
@@ -1,15 +1,15 @@
 #create a function for implementing a exponential decay probability distribution
-exp.prob <- function(X, amp, lambda, probability=F){
+exp.prob <- function(X, amp, lambda, x_0 = 0.0, probability=F){
   if(probability==T && missing(amp)){
     #This is the probability distribution
-    Y <- (lambda/2)*exp(-lambda*abs(X))
+    Y <- (lambda/2)*exp(-lambda*abs(X - x_0))
   }else if(probability==T && !(missing(amp))){
-    stop("The value of amp parameter doesn't matter in an exponential decay distribution")
+    stop("The value of amp parameter doesn't matter in a probability distribution")
   }else if(probability==F && missing(amp)){
     stop("No value of amp(scaling factor) parameter")
   }else{
     ##This is simply a statistical distribution
-    Y <- amp*exp(-lambda*abs(X))
+    Y <- amp*exp(-lambda*abs(X - x_0))
   }
   return(Y)
 }
